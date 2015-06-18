7be26a4a7878063788470789d0cbb0448b5806d4
Raúl Pérula-Martínez <raules@gmail.com>
2015-06-18 14:09:56 +0200

    testing initialpose

--- maggie_navigation_config/src/initial_status_sender.py
+++ maggie_navigation_config/src/initial_status_sender.py
@@ -71,9 +71,11 @@ class InitialposeSender():
                 msg.pose.pose.orientation.x = 0
                 msg.pose.pose.orientation.y = 0
                 print place_data['th']
-                msg.pose.pose.orientation.z = math.sin(place_data['th'])
+                msg.pose.pose.orientation.z = math.sin(
+                    (place_data['th'] - math.pi / 2.0) / 2.0)
                 print math.sin(place_data['th'])
-                msg.pose.pose.orientation.w = math.cos(place_data['th'])
+                msg.pose.pose.orientation.w = math.cos(
+                    (place_data['th'] - math.pi / 2.0) / 2.0)
                 print math.cos(place_data['th'])
 
                 # publish the initial position
